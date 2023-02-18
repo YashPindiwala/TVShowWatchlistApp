a6c85f493fd21ab8dbf7ea113fc67b7706f942b0
YashPindiwala <w0790912@myscc.ca>
2023-02-18 13:02:57 -0500

    Created custom cell class

---
+++ Test1/CustomTableCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableCell.swift
+//  Test1
+//
+//  Created by yash pindiwala on 2023-02-18.
+//
+
+import UIKit
+
+class TVShowCell: UITableViewCell{
+    @IBOutlet var trackName: UILabel!
+    @IBOutlet var shortDesc: UILabel!
+    @IBOutlet var collectionName: UILabel!
+    @IBOutlet var trackPrice: UILabel!
+}
